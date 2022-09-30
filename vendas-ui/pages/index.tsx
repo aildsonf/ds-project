5ac05a60e48bbfe2ac18fbff83977ab7d8fedb89
Aildson Ferreira <aildson.ferreira@liferay.com>
2022-09-29 22:46:15 -0300

    Initial configuration

---
+++ vendas-ui/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className='font-extrabold'>
+      Hello World
+    </div>
+  )
+}
+
+export default Home
