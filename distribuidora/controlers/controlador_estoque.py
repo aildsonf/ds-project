54352782e1d2ce0f11fdea38cf3a29eaf3c26e8d
GianBarbosa <gianfelipe.21@gmail.com>
2022-10-03 19:23:51 -0300

    Comunicação da distribuidora usando rabbitmq

--- distribuidora/controlers/controlador_estoque.py
+++ distribuidora/controlers/controlador_estoque.py
@@ -1,66 +1,60 @@
 import sqlite3
 
-#Produto = id(int), nome(string), qtd(int)
 class ControladorDeEstoque:
     # conecta ao banco de dados
     # cria os elementos se necessario
     def __init__(self):
         self.estoque = sqlite3.connect('distribuidora.db', check_same_thread=False)
         cursor = self.estoque.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS produtos (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome VARCHAR(255), qtd INTEGER UNSIGNED, preco INTEGER)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS produtos (barcode VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, brand VARCHAR(255) NOT NULL, weight FLOAT(10,7), price FLOAT(10,7) NOT NULL, qtd INTEGER UNSIGNED)")
 
     # encerra a conecxão com o banco de dados
     def close(self):
         self.estoque.close()
 
-    # recebe o nome do produto e a quantidade
     # adiciona um novo produto ao estoque
-    def add_novo_produto(self, nome_produto, qtd, preco):
+    def add_novo_produto(self, barcode, name, brand, weight, price, qtd):
         cursor = self.estoque.cursor()
-        sql = "INSERT INTO produtos (nome, qtd, preco) VALUES(?, ?, ?)"
-        val = (nome_produto, qtd, preco)
+        sql = "INSERT INTO produtos (barcode, name, brand, weight, price, qtd) VALUES(?, ?, ?, ?, ?, ?)"
+        val = (barcode, name, brand, weight, price, qtd)
         cursor.execute(sql, val)
         self.estoque.commit()
 
-    # recebe o id do prdoduto(int), e a quantidade(int)
     # aumenta a quantidade do produto no estoque
-    def add_produto(self, produto_id, qtd):
+    def add_produto(self, barcode, qtd):
         cursor = self.estoque.cursor()
-        sql = "UPDATE produtos SET qtd = qtd + ? WHERE id = ?"
-        val = (qtd, produto_id)
+        sql = "UPDATE produtos SET qtd = qtd + ? WHERE barcode = ?"
+        val = (qtd, barcode)
         cursor.execute(sql, val)
         self.estoque.commit()
 
-    # recebe o id do prdoduto(int), e a quantidade(int)
     # reduz a quantidade do produto no estoque
-    def retirar_produto(self, produto_id, qtd):
+    def retirar_produto(self, barcode, qtd):
         cursor = self.estoque.cursor()
-        sql = "UPDATE produtos SET qtd = qtd - ? WHERE id = ?"
-        val = (qtd, produto_id)
+        sql = "UPDATE produtos SET qtd = qtd - ? WHERE barcode = ?"
+        val = (qtd, barcode)
         cursor.execute(sql, val)
         self.estoque.commit()
 
-    # revome um produto do estoque a partir do id
-    def remove_produto(self, produto_id):
+    def remove_produto(self, barcode):
         cursor = self.estoque.cursor()
-        sql = "DELETE FROM produtos WHERE id = ?"
-        val = (produto_id,)
+        sql = "DELETE FROM produtos WHERE barcode = ?"
+        val = (barcode,)
         cursor.execute(sql, val)
         self.estoque.commit()
 
     # retorna um produto de acordo com o id
-    def buscar_produto(self, produto_id):
+    def buscar_produto(self, barcode):
         cursor = self.estoque.cursor()
-        sql = "SELECT * FROM produtos WHERE id = ?"
-        val = (produto_id,)
+        sql = "SELECT * FROM produtos WHERE barcode = ?"
+        val = (barcode,)
         cursor.execute(sql, val)
         return cursor.fetchone()
 
-    # retorna um produto de acordo com o id
-    def buscar_produto_nome(self, produto_nome):
+    def listar_produto_nome(self, name):
         cursor = self.estoque.cursor()
-        sql = "SELECT * FROM produtos WHERE id = %?%"
-        val = (produto_nome,)
+        sql = "SELECT * FROM produtos WHERE name = %?%"
+        val = (name,)
         cursor.execute(sql, val)
         return cursor.fetchall()
 
@@ -73,14 +67,14 @@ class ControladorDeEstoque:
 
     # recebe o id do produto e a quantidade
     # retorna True se o estoque possui a quantidade solicitada do produto
-    def has_estoque(self, produto_id, qtd):
+    def has_estoque(self, barcode, qtd):
         cursor = self.estoque.cursor()
-        sql = "SELECT * FROM produtos WHERE id = ?"
-        val = (produto_id,)
+        sql = "SELECT * FROM produtos WHERE barcode = ?"
+        val = (barcode,)
         cursor.execute(sql, val)
         result = cursor.fetchone()
         try:
-            if result[2] >= qtd:
+            if result[-1] >= qtd:
                 return True
         except:
             pass
--- distribuidora/gerenciador.py
+++ distribuidora/gerenciador.py
@@ -1,9 +1,13 @@
 import os
 import threading
 import time
+import random
+from fastapi import APIRouter
+import pika
 from controlers.controlador_estoque import ControladorDeEstoque
 from controlers.controlador_pedidos import ControladorDePedidos
 
+router = APIRouter()
 
 # classe central de controle de execução
 # executa a thread principal
@@ -21,46 +25,53 @@ class Gerenciador:
     # se está em falta, solicita reestoque dos produtos se o pedido já não está esperando pelo reestoque
     def _tratar_pedido(self, pedido):
         em_falta = False
-        for produto in pedido[2]:
+        for produto in pedido[1]:
             if not self._estoque.has_estoque(produto[0], produto[1]):
                 em_falta = True
         if not em_falta:
-            for produto in pedido[2]:
+            for produto in pedido[1]:
                 self._estoque.retirar_produto(produto[0], produto[1])
             # envia o pedido/proodutos
-            pedido[3] = "Enviado"
-            # # sender
-            # # connects to the broker
-            # connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-            # channel = connection.channel()
-            #
-            # # verifica se ja foi declarado, se não cria
-            # channel.exchange_declare(exchange="Distribuidora", exchange_type="direct", durable=True)
-            # channel.queue_declare(queue="Pedidos", durable=True)
-            # # channel.queue_bind(queue="Pedidos", exchange="Distribuidora", routing_key="PeA")
-            #
-            # channel.basic_publish(exchange="Distribuidora", routing_key="PeA", body='[(1, 100)]',
-            #                       properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE))
+            pedido[2] = "Enviado"
+            # sender
+            # connects to the broker
+            credentials = pika.PlainCredentials('guest', 'guest')
+            connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', 5672, '/', credentials))
+            channel = connection.channel()
+
+            # verifica se ja foi declarado, se não cria
+            channel.exchange_declare(exchange="Distribuidora", exchange_type="direct", durable=True)
+            channel.queue_declare(queue="Produtos-" + str(pedido[0]), durable=True)
+            channel.queue_bind(queue="Produtos-" + str(pedido[0]), exchange="Distribuidora", routing_key="Pr" + str(pedido[0]))
+
+            #envia a mensagem(body = str(pedido[1]))
+            channel.basic_publish(exchange="Distribuidora", routing_key="Pr" + str(pedido[0]), body=str(pedido[1]),
+                                  properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE))
+
             self._pedidos.finalizar_pedido(pedido)
         else:
-            if pedido[3] != "Espera":
-                for produto in pedido[2]:
+            if pedido[2] != "Espera":
+                for produto in pedido[1]:
                     # Se não possui estoque do produto faz o reestoque de 100 unidades do produto
                     if not self._estoque.has_estoque(produto[0], produto[1]):
                         produto = self._estoque.buscar_produto(produto[0])
-                        if produto: print("Não possui estoque suficiente do produto \"{0}\" id {1} para realizar o pedido {2}".format(produto[1], produto[0], pedido[0]))
-                pedido[3] = "Espera"
+                        if produto: print("Não possui estoque suficiente do produto \"{0}\" codigo de barra {1} para realizar o pedido {2}".format(produto[1], produto[0], pedido[0]))
+                pedido[2] = "Espera"
                 self._pedidos.salvar()
             self._pedidos.to_last()
 
+    @router.post("/ditrA/produtos")
+    def listar_produtos(self):
+        return self._estoque.listar_produtos()
+
     # recebe o id do prdoduto(int), e a quantidade(int)
     # solicita o reestoque de um produto
-    def reestoque(self, produto_id, qtd):
+    def reestoque(self, barcode, qtd):
 
         # faz o pagamento antes de adicionar ao estoque
         ...
 
-        self._estoque.add_produto(produto_id, qtd)
+        self._estoque.add_produto(barcode, qtd)
 
     # ativa a condição de parada da thread
     def stop(self):
@@ -113,18 +124,26 @@ class Gerenciador:
                     print("Pressione algo para continuar")
                     input()
                 elif x == 'N' or x == 'n':
+                    print("Digite o barcode do produto: ")
+                    barcode = input()
                     print("Digite o nome do produto: ")
-                    nome = input()
-                    print("Digite a quantidade: ")
-                    qtd = input()
+                    name = input()
+                    print("Digite a marca do produto: ")
+                    brand = input()
+                    print("Digite o peso do produto: ")
+                    weight = float(input())
                     print("Digite o preço: R$")
-                    preco = input()
-                    self._estoque.add_novo_produto(nome, qtd, preco)
-                elif x == 'R' or x == 'r':
-                    print("Digite o id do produto: ")
-                    produto_id = input()
+                    price = float(input())
                     print("Digite a quantidade: ")
                     qtd = input()
-                    self.reestoque(produto_id, qtd)
+                    self._estoque.add_novo_produto(barcode, name, brand, weight, price, qtd)
+                    # self._estoque.add_novo_produto("01", "monitor", "multi", 3.2, 400.00, 100)
+                    # self._estoque.add_novo_produto("02", "teclado", "multi", 2.0, 100.0, 100)
+                elif x == 'R' or x == 'r':
+                    print("Digite o baarcode do produto: ")
+                    barcode = input()
+                    print("Digite a quantidade: ")
+                    qtd = input()
+                    self.reestoque(barcode, qtd)
             else:
                 break
